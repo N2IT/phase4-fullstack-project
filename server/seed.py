00b13e4b7b039d0a2bf0d267cbd56950ca8c6341
Tony Eder <tonyeder11@gmail.com>
2024-05-16 11:47:25 -0700

    have now seeded users table. need to add in passwords and related hash validations. Like accounts, need to address the updated_at data as it is not correct. but good start

--- server/seed.py
+++ server/seed.py
@@ -29,6 +29,24 @@ def create_accounts():
   return accounts
 
 
+def create_users():
+  users = []
+  for _ in range(50):
+    u = User(
+      first_name = fake.first_name(),
+      last_name = fake.last_name(),
+      username=fake.profile(fields=['username'])['username'],
+      created_at = datetime.now(),
+      updated_at = datetime.now(),
+      status = fake.boolean(chance_of_getting_true=75),
+      account_id = rc([account.id for account in accounts])
+    )
+  
+    users.append(u)
+
+  return users
+
+
 if __name__ == "__main__":
   with app.app_context():
     print("Clearing db...")
@@ -37,7 +55,11 @@ if __name__ == "__main__":
     print("Seeding accounts...")
     accounts = create_accounts()
     db.session.add_all(accounts)
-    breakpoint()
+    db.session.commit()
+
+    print('Seeding users...')
+    users = create_users()
+    db.session.add_all(users)
     db.session.commit()
 
     print('done seeding')
