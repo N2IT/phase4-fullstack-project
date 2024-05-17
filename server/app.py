ac309072ccf3ed9296ddb3856df6fa80012a0fb7
Tony Eder <tonyeder11@gmail.com>
2024-05-17 11:23:52 -0700

    create user also working via postman

--- server/app.py
+++ server/app.py
@@ -66,15 +66,73 @@ class CreateAccount(Resource):
       db.session.add(new_account)
       db.session.commit()
 
+      session['account_id'] = new_account.id
+
       return new_account.to_dict(), 201
 
     else:
       return {'errors' : '422: Unprocessable Entry'}, 422
 
+class CheckSession(Resource):
+  def get(self):
+    if session['account_id']:
+      account = Account.query.filter(Account.id == session.get('account_id')).first()
+      return account.to_dict(), 200
+    elif session['user_id']:
+      user = User.query.filter(User.id == session.get('user_id')).first()
+      return user.to_dict(), 200
+    else:
+      return {'errors' : '401 : Unauthorized'}, 401
+
+class CreateUser(Resource):
+  def post(get):
+    form_data = request.get_json()
+    
+    first_name = form_data.get('first_name')
+    last_name = form_data.get('last_name')
+    username = form_data.get('username')
+
+    errors = []
+
+    if form_data:
+      if not first_name:
+        errors.append('A first name must be entered.')
+      elif not last_name:
+        errors.append('An last name must be entered.')
+      elif not username:
+        errors.append('A username must be entered.')
+      elif User.query.filter(User.username == username).first():
+        errors.append('The username name must be unique.')
+      
+      if errors:
+        return {'errors' : errors }, 422
+      
+      if 'username' in form_data:
+        new_user = User(
+          first_name = first_name,
+          last_name = last_name,
+          username = username
+        )
+
+        new_user.password_hash = form_data['password']
+
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        session['user_id'] = new_user.id
+
+        return new_user.to_dict(), 201
+
+    else:
+      return {'errors' : '422: Unprocessable Entry'}, 422
+
+
 
 api.add_resource(Accounts, '/accounts')
 api.add_resource(Users, '/users')
 api.add_resource(CreateAccount, '/create_account')
+api.add_resource(CreateUser, '/create_user')
 
 
 if __name__ == "__main__":
