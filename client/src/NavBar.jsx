6d1a00482a6fc42850ffa9e6489ab28c900bd068
Tony Eder <tonyeder11@gmail.com>
2024-05-22 10:53:59 -0700

    refactored NavBar code to remove commented out portions

--- client/src/NavBar.jsx
+++ client/src/NavBar.jsx
@@ -1,95 +1,3 @@
-// // import Button from 'react-bootstrap/Button';
-// import Container from 'react-bootstrap/Container';
-// // import Form from 'react-bootstrap/Form';
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
-// // import NavDropdown from 'react-bootstrap/NavDropdown';
-// import Offcanvas from 'react-bootstrap/Offcanvas';
-// import { Link, useNavigate } from 'react-router-dom'
-// import { useState } from 'react';
-
-// function NavBar({ user, setUser }) {
-//     const navigate = useNavigate();
-//     const [expanded, setExpanded] = useState(false);
-
-
-//     const handleLogoutClick = () => {
-//         fetch("/api/logout", {
-//             method: 'DELETE'
-//         })
-//             .then((r) => {
-//                 if (r.ok) {
-//                     setUser(null)
-//                     navigate('/')
-//                 }
-//             })
-//     }
-
-//     return (
-//         <>
-//             {user ?
-//                 ([false].map((expand) => (
-//                     <Navbar expanded={expanded} key={expand} expand={expand} className="bg-body-tertiary mb-3">
-//                         <Container fluid>
-//                             <Navbar.Brand href="/">qp</Navbar.Brand>
-//                             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} onClick={() => setExpanded(expanded ? false : "expanded")}  />
-//                             <Navbar.Offcanvas
-//                                 id={`offcanvasNavbar-expand-${expand}`}
-//                                 aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-//                                 placement="end"
-//                             >
-//                                 <Offcanvas.Header closeButton onClick={() => setExpanded(false)}>
-//                                     <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-//                                         QuotePro
-//                                     </Offcanvas.Title>
-//                                 </Offcanvas.Header>
-//                                 <Offcanvas.Body>
-//                                     <Nav className="justify-content-end flex-grow-1 pe-3">
-//                                         <Link 
-//                                             to="/" 
-//                                             onClick={() => setExpanded(false)}
-//                                             >Home
-//                                         </Link>
-//                                         <Link 
-//                                             to="/accounts"
-//                                             onClick={() => setExpanded(false)}
-//                                             >Accounts
-//                                         </Link>
-//                                         <Link 
-//                                             to="/users"
-//                                             onClick={() => setExpanded(false)}
-//                                             >Users
-//                                         </Link>
-//                                         <Link 
-//                                             to="/quotes"
-//                                             onClick={() => setExpanded(false)}
-//                                             >Quotes
-//                                         </Link>
-//                                         <Link 
-//                                             to="/new-quote"
-//                                             onClick={() => setExpanded(false)}
-//                                             >New Quote
-//                                         </Link>
-//                                         <Link 
-//                                             to="/support"
-//                                             onClick={() => setExpanded(false)}
-//                                             >Support
-//                                         </Link>
-//                                         <hr />
-//                                         <button onClick={handleLogoutClick}>Logout</button>
-//                                     </Nav>
-//                                 </Offcanvas.Body>
-//                             </Navbar.Offcanvas>
-//                         </Container>
-//                     </Navbar >
-//                 ))
-//                 ) : null}
-//         </>
-//     );
-// }
-
-// export default NavBar;
-
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -103,7 +11,7 @@ function NavBar({ user, setUser }) {
 
   useEffect(() => {
     if (user) {
-      setExpanded(false); // Collapse the menu after login or account creation
+      setExpanded(false);
     }
   }, [user]);
 
