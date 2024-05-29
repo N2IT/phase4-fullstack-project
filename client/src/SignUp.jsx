1078be903372e0a086a0d17ea4bd9d50667a4df3
Tony Eder <tonyeder11@gmail.com>
2024-05-28 20:16:53 -0700

    updated: outlet context for app.jsx, Signup.jsx, NewUserForm & NewAccountForm. Pushing props to app.jsx file

--- client/src/SignUp.jsx
+++ client/src/SignUp.jsx
@@ -1,14 +1,12 @@
 import NewUserForm from "./NewUserForm";
 import NewAccountForm from "./NewAccountForm";
-import { useState } from "react";
+// import { useState } from "react";
+import { useOutletContext } from "react-router-dom";
 
 const SignUp = () => {
-    const[accountForm, setAccountForm] = useState(true)
 
-    const onSubmitAccountForm = () => {
-        setAccountForm(!accountForm)
-    }
-
+    const [user, setUser, accounts, setAccounts, accountForm, setAccountForm, onSubmitAccountForm] = useOutletContext()
+    
     return (
       <div>
         {/* <h2>Create New Account</h2> */}
