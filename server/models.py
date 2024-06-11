3845623e59bda1eb7a92b1f06f0a254ca6bfb611
Tony Eder <tonyeder11@gmail.com>
2024-06-11 12:06:16 -0700

    rolled back and updated prev migrations just to test building objects and adding to tables. Now will work on relationships

--- server/models.py
+++ server/models.py
@@ -41,8 +41,8 @@ class Account(db.Model, SerializerMixin):
     
     # relationships
     users = db.relationship('User', back_populates = 'account')
-    customers = db.relationship('Customer', back_populates = 'account')
-    quotes = db.relationship('Quote', back_populates = 'account')
+    # customers = db.relationship('Customer', back_populates = 'account')
+    # quotes = db.relationship('Quote', back_populates = 'account')
 
     serialize_rules = ('-users.account', '-users.created_at', '-users.updated_at', '-users._password_hash', '-customers', '-quotes')
 
@@ -164,17 +164,18 @@ class Customer(db.Model, SerializerMixin):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
     updated_by = db.Column(db.Integer)
     notes = db.Column(db.String(500))
-    account_id = db.Column(db.Integer, db.ForeignKey('accounts.id'))
+    # account_id = db.Column(db.Integer, db.ForeignKey('accounts.id'))
 
     ## relationships
-    quotes = db.relationship('Quote', back_populates = 'customer')
-    account = db.relationship('Account', back_populates = 'customers')
+    # quotes = db.relationship('Quote', back_populates = 'customer')
+    # account = db.relationship('Account', back_populates = 'customers')
 
     ##serialize
-    serialize_rules = ('-account','-quotes',)
+    # serialize_rules = ('-quotes',)
 
     def __repr__(self):
-        return f'Customer {self.id}, {self.first_name}, {self.last_name}, {self.email}, {self.phone}, {self.created_at}, {self.created_by}, {self.updated_at}, {self.updated_by}, {self.notes}, {self.account_id}'
+        return f'Customer {self.id}, {self.first_name}, {self.last_name}, {self.email}, {self.phone}, {self.created_at}, {self.created_by}, {self.updated_at}, {self.updated_by}, {self.notes} '
+
 
 
 class Configuration(db.Model, SerializerMixin):
@@ -186,10 +187,10 @@ class Configuration(db.Model, SerializerMixin):
     cost = db.Column(db.Integer)
 
     ##relationships
-    quote = db.relationship('Quote', back_populates = 'configurations')
+    # quote = db.relationship('Quote', back_populates = 'configurations')
 
     ##serialize
-    serialize_rules = ('-quote',)
+    # serialize_rules = ('-quote',)
 
     def __repr__(self):
         return f'Configuration {self.id}, {self.sku}, {self.product_title}, {self.product_description}, {self.cost}'
@@ -213,16 +214,16 @@ class Quote(db.Model, SerializerMixin):
     created_by = db.Column(db.Integer)
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
     updated_by = db.Column(db.Integer)
-    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
-    configuration_id = db.Column(db.Integer, db.ForeignKey('configurations.id'))
-    account_id = db.Column(db.Integer, db.ForeignKey('accounts.id'))
+    # customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    # configuration_id = db.Column(db.Integer, db.ForeignKey('configurations.id'))
+    # account_id = db.Column(db.Integer, db.ForeignKey('accounts.id'))
 
     # relationships
-    customer = db.relationship('Customer', back_populates = 'quotes')
-    configurations = db.relationship('Configuration', back_populates = 'quote')
+    # customer = db.relationship('Customer', back_populates = 'quotes')
+    # configurations = db.relationship('Configuration', back_populates = 'quote')
 
     ##serialize
-    serialize_rules = ('-customer', '-configurations',)
+    # serialize_rules = ('-customer', '-configurations',)
 
     def __repr__(self):
-        return f'Quote {self.id}, {self.quote_number}, {self.title}, {self.discount}, {self.savings}, {self.markup_variable}, {self.sale_price}, {self.margin_percentage}, {self.margin_dollars}, {self.notes}, {self.status}, {self.converted}, {self.created_at}, {self.created_by}, {self.updated_at}, {self.updated_by}, {self.customer_id}, {self.configuration_id}, {self.account_id}'
+        return f'Quote {self.id}, {self.quote_number}, {self.title}, {self.discount}, {self.savings}, {self.markup_variable}, {self.sale_price}, {self.margin_percentage}, {self.margin_dollars}, {self.notes}, {self.status}, {self.converted}, {self.created_at}, {self.created_by}, {self.updated_at}, {self.updated_by}'
