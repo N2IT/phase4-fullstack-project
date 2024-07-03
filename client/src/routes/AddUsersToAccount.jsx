586b95943f61f1f7edb1902c1679269026fbd650
Tony Eder <tonyeder11@gmail.com>
2024-07-03 15:12:09 -0700

    corrected the admin ability to override adding users to accounts that do not exist. now for the customers

--- client/src/routes/AddUsersToAccount.jsx
+++ client/src/routes/AddUsersToAccount.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import { useParams } from "react-router-dom";
 import { AgentContext } from '../AgentProvider';
 import CreateNewUserForm from '../components/CreateNewUserForm';
@@ -7,8 +7,28 @@ import Unauthorized from "../components/Unauthorized";
 
 const AddUsersToAccount = () => {
 
-    const { agent, isLoading } = useContext(AgentContext);
+    const { agent, setAccount, setErrors, setAsDisabled, account, isLoading, errors } = useContext(AgentContext);
     const { id } = useParams()
+
+    useEffect(() => {
+        fetch(`/api/accounts/${id}`)
+            .then(response => {
+                if (!response.ok) {
+                    return response.json().then(data => { throw data; });
+                }
+                return response.json();
+            })
+            .then(data => {
+                setAccount(data);
+                setAsDisabled(true);
+                setErrors(null);
+            })
+            .catch(error => {
+                console.error('Errors:', error);
+                setErrors([error.errors] || ['Unknown Error']);
+                setAccount(null);
+            });
+    },[id,agent])
 
     if (isLoading) {
         return <div>Loading ...</div>;
@@ -20,13 +40,19 @@ const AddUsersToAccount = () => {
         )
     }
 
-    if (agent.role_id === 1) {
+    if (agent.role_id === 1 && errors) {
+        alert('Cmon admin! You cant get away with that!')
+        history.go(-1)
+    }
+
+    if (agent.role_id === 1 ) {
         return (
             <div>
                 <CreateNewUserForm />
             </div>
         );
     }
+
 
     if (agent.role_id === 2 && agent.account_id.toString() === id) {
         return (
