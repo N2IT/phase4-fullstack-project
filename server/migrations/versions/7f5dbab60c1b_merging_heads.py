222ef41454a4bfdf53fb7d5d954fc0a8be525e5c
Tony Eder <tonyeder11@gmail.com>
2024-07-17 09:54:16 -0700

    commiting to save

---
+++ server/migrations/versions/7f5dbab60c1b_merging_heads.py
@@ -0,0 +1,24 @@
+"""Merging heads
+
+Revision ID: 7f5dbab60c1b
+Revises: 11f94b7aaf63, da28c6bc2d0c
+Create Date: 2024-07-17 09:44:45.211036
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7f5dbab60c1b'
+down_revision = ('11f94b7aaf63', 'da28c6bc2d0c')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- server/models.py
+++ server/models.py
@@ -17,7 +17,7 @@ class Account(db.Model, SerializerMixin):
     city = db.Column(db.String)
     state = db.Column(db.String)
     zip_code = db.Column(db.Integer)
-    phone = db.Column(db.Integer)
+    phone = db.Column(db.String)
     discount = db.Column(db.Integer)
     status = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
@@ -67,7 +67,7 @@ class User(db.Model,SerializerMixin):
     status = db.Column(db.String)
     account_id = db.Column(db.Integer, db.ForeignKey('accounts.id'), nullable = False)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-    _password_hash = db.Column(db.String(12))
+    _password_hash = db.Column(db.String)
 
     @hybrid_property
     def password_hash(self):
