0e86fed572636e3e4631f8bd4059d9fbdc845618
Tony Eder <tonyeder11@gmail.com>
2024-07-22 13:02:40 -0700

    corrected typo

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    cores: true,
+    cors: true,
   //  proxy: {
   //   '/api': {
   //     target: 'http://127.0.0.1:5555',
