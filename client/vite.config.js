acb9ffa951b7c8921cbaeb362f8270603ead8f7e
Tony Eder <tonyeder11@gmail.com>
2024-07-23 10:12:29 -0700

    updated pipfile, added Procfile

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,12 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-   proxy: {
-    '/api': {
-      target: 'http://127.0.0.1:5555',
-      changeOrigin: true,
-      rewrite: (path) => path.replace(/^\/api/, ''),
+    port: 3000,
+    cors: true,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:5050',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
     },
-   },
   }
 })
