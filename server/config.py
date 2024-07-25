3ce4a165c94bd0c58163380b95305b25dc42f08b
Tony Eder <tonyeder11@gmail.com>
2024-07-25 10:47:53 -0700

    settings back to local for

--- server/config.py
+++ server/config.py
@@ -22,15 +22,15 @@ naming_convention = {
 metadata = MetaData(naming_convention=naming_convention)
 
 # COMMENT OUT FOR PRODUCTION
-# app = Flask(__name__)
+app = Flask(__name__)
 
 #COMMENT OUT FOR DEVELOPMENT
-app = Flask(
-    __name__,
-    static_url_path='',
-    static_folder='../client/dist',
-    template_folder='../client/dist'
-)
+# app = Flask(
+#     __name__,
+#     static_url_path='',
+#     static_folder='../client/dist',
+#     template_folder='../client/dist'
+# )
 
 # app = Flask(
 #     __name__,
